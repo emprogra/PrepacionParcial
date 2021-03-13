9f315e1f178f259064d911471bd5af832c9cf1d5
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-13 11:38:26 -0400

    Modelo Incorporado

---
+++ EvalEmilyRoss/ApiCountry/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiCountry.Models
+{
+    public class Country
+    {   
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public List<double> LatIng { get; set; }
+        public List<string> TimeZones { get; set; }
+        public List<Currency> Curriences { get; set; }
+        public string FLag { get; set; }
+
+
+
+
+    }
+}

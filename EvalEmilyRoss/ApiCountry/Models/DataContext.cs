9f966ce1bb735ee81b77f234fd173ac5cbd8ef35
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-18 20:20:57 -0400

    programa completo

--- EvalEmilyRoss/ApiCountry/Models/DataContext.cs
+++ EvalEmilyRoss/ApiCountry/Models/DataContext.cs
@@ -12,5 +12,9 @@ namespace ApiCountry.Models
         {
 
         }
+
+        public System.Data.Entity.DbSet<ApiCountry.Models.Currency> Currencies { get; set; }
+
+        public System.Data.Entity.DbSet<ApiCountry.Models.Country> Countries { get; set; }
     }
 }
